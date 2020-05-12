6c8fa367b7591043fbd108390ff1c72a7d42797a
liyunjuan <13272540760@163.com>
2020-05-12 21:17:08 +0800

    fixe

---
+++ vNode.js
@@ -0,0 +1,8 @@
+export default class VNode {
+    constructor(tag,data,children,text,elm,context,componentOptions,asyncFactory) {
+        this.tag = tag
+        this.data = data
+        this.children = children
+        
+    }
+}
